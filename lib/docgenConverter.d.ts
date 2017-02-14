3000fba62b6f3acb09a5f6294254685b8a4ee6c1
Michal Mondik <m.mondik@gmail.com>
2017-02-14 18:09:32 +0100

    Add libs

---
+++ lib/docgenConverter.d.ts
@@ -0,0 +1,22 @@
+import { FileDoc } from './parser';
+export declare function convertToDocgen(doc: FileDoc): {
+    description: string;
+    props: {};
+};
+export interface PropItemType {
+    name: string;
+    value?: any;
+}
+export interface PropItem {
+    required: boolean;
+    type: PropItemType;
+    description: string;
+    defaultValue: any;
+}
+export interface PropsObject {
+    [key: string]: PropItem;
+}
+export interface Docgen {
+    description: string;
+    props: PropsObject;
+}
---
+++ lib/propTypesParser.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Parser given file and return documentation in format compatibe with react-docgen.
+ */
+export declare function parse(filePath: string): {
+    description: string;
+    props: {};
+};
